429a434c1b6f0f148d94a91afd90a5c47581d5d8
David Rabko <davidryabko13@gmail.com>
2024-01-06 16:09:58 +0100

    Add platinum refactoring

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,10 +2,11 @@ import './card-list.styles.css'
 import Card from "../card/card.component";
 
 const CardList = ({ monsters }) => (
-    <div className='card-list'>
-        { monsters.map((monster) => {
+    <div className='card-list'>{
+        monsters.map((monster) => {
             return <Card monster={ monster }/>
-        }) }
+        })
+    }
     </div>
 );
 
